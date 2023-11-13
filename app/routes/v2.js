ce8c026d7125c7b673054df7dd3a7ec2e6cd2c12
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-13 14:43:23 -0500

    v2.1

--- app/routes/v2.js
+++ app/routes/v2.js
@@ -9,14 +9,18 @@ router.get('/', async function(req, res, next) {
         const response = await axios.get(computeUrl, { withCredentials: true });
 
         // Forward the cookies received from compute.1var.com to the user
+        console.log("a")
         const cookies = response.headers['set-cookie'];
+        console.log("b")
         if (cookies) {
+            console.log("c")
             cookies.forEach(cookie => {
+                console.log(cookie)
                 res.setHeader('Set-Cookie', cookie);
             });
         }
 
-        res.render('v2', { title: 'v2' });
+        res.render('v2', { title: 'v2.1' });
     } catch (error) {
         console.error('Error calling compute.1var.com:', error);
         res.status(500).send('Server Error');
