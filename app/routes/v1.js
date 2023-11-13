8a919dbc427819c34489c02f21bde5ff18ddb7d8
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-13 14:18:55 -0500

    v to v1

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async function(req, res, next){
-    res.render('v`', {})
+    res.render('v1`', {})
 });
 
 // Example Environment Variable: process.env.DB_CLUSTER_CONN_STR
