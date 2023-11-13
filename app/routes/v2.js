a37257bca9fe40fa26a32b623287cbf4dab14b7d
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-13 16:54:47 -0500

    CORS headers added

--- app/routes/v2.js
+++ app/routes/v2.js
@@ -4,6 +4,9 @@ const axios = require('axios');
 
 router.get('/', async function(req, res, next) {
     try {
+        res.header('Access-Control-Allow-Origin', 'http://1var.com'); // Replace with your client's URL
+        res.header('Access-Control-Allow-Credentials', 'true');
+
         const computeUrl = 'https://compute.1var.com/cookies';
         const response = await axios.get(computeUrl, { withCredentials: true });
 
