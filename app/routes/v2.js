4d927bb655b8d6f2b57cf0597752f792aee98b87
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-14 11:20:51 -0500

    logging type

--- app/routes/v2.js
+++ app/routes/v2.js
@@ -6,9 +6,9 @@ router.get('/', async function(req, res, next) {
     try {
         res.header('Access-Control-Allow-Origin', 'https://1var.com'); // Replace with your client's URL
         res.header('Access-Control-Allow-Credentials', 'true');
-
         const type = req.type; // Get the type from the request
         const computeUrl = `https://compute.1var.com/${type}`;
+        console.log("type",type)
         const response = await axios.get(computeUrl, { withCredentials: true });
 
         const cookies = response.headers['set-cookie'];
