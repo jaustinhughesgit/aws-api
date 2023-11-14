ab2738cc858d9f9b5b352146c5cebd1322bbe52b
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-14 12:17:53 -0500

    using gpts code

--- app/routes/v2.js
+++ app/routes/v2.js
@@ -8,12 +8,13 @@ router.get('/', async function(req, res, next) {
         res.header('Access-Control-Allow-Credentials', 'true');
         const type = req.type; // Get the type from the request
         const computeUrl = `https://compute.1var.com/${type}`;
-        console.log("type",type)
+        console.log("type", type);
         const response = await axios.get(computeUrl, { withCredentials: true });
 
-        if (type == "url"){
-            res.render(response);
-        } else {
+        if (type === "url") {
+            // Assuming the response from computeUrl is the data you want to send as JSON
+            res.json(response.data);
+        } else if (type === "cookies") {
             const cookies = response.headers['set-cookie'];
             if (cookies) {
                 cookies.forEach(cookie => {
@@ -21,7 +22,10 @@ router.get('/', async function(req, res, next) {
                 });
             }
             res.render('v2', { title: 'v2.3' });
-        }   
+        } else {
+            // Handle unexpected type
+            res.status(400).send('Invalid type');
+        }
 
     } catch (error) {
         console.error('Error calling compute.1var.com:', error);
