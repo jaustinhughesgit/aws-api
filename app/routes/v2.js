225708e24827f9c17ebd520014d0e2156c7e65ee
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-29 10:24:06 -0500

    logging req

--- app/routes/v2.js
+++ app/routes/v2.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 console.log("vsRouter1")
 router.get('/*', async function(req, res, next) {
     console.log("vsRouter2")
+    console.log("req",req)
     try {
         res.header('Access-Control-Allow-Origin', 'https://1var.com');
         res.header('Access-Control-Allow-Credentials', 'true');
