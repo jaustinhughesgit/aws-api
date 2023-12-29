ef2bc9be77287e6c1c5bbb9ff3d55c02680ba569
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-29 02:58:42 -0500

    WORKING

--- app/routes/v2.js
+++ app/routes/v2.js
@@ -5,15 +5,13 @@ console.log("vsRouter1")
 router.get('/', async function(req, res, next) {
     console.log("vsRouter2")
     try {
-        res.header('Access-Control-Allow-Origin', 'https://1var.com'); // Replace with your client's URL
+        res.header('Access-Control-Allow-Origin', 'https://1var.com');
         res.header('Access-Control-Allow-Credentials', 'true');
         console.log("vsRouter3")
-        const type = req.type; // Get the type from the request
+        const type = req.type; 
         const computeUrl = `https://compute.1var.com/${type}`;
-        console.log("type", type);
         const response = await axios.get(computeUrl, { withCredentials: true });
         if (type === "url") {
-            // Assuming the response from computeUrl is the data you want to send as JSON
             res.json(response.data);
         } else if (type === "cookies") {
             console.log("set cookies")
@@ -23,10 +21,8 @@ router.get('/', async function(req, res, next) {
                     res.append('Set-Cookie', cookie);
                 });
             }
-            console.log("SUCCESS SUCCESS SUCCESS")
             res.send(response.data);
         } else {
-            // Handle unexpected type
             res.status(400).send('Invalid type');
         }
 
