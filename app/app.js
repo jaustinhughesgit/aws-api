02086e1f452a4dd366c3248cce24f88ddf2a5c2f
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-29 01:10:20 -0500

    moved originalHost to app.js

--- app/app.js
+++ app/app.js
@@ -12,6 +12,9 @@ var v2Router = require('./routes/v2');
 app.use('/', indexRouter);
 // Route for /cookies/* and /url/*
 app.use('/:type(cookies|url)', function(req, res, next) {
+
+    const originalHost = req.headers['x-forwarded-host'];
+    console.log("originalHost2",originalHost)
     req.type = req.params.type; // Capture the type (cookies or url)
     next('route'); // Pass control to the next route
 }, v2Router);
--- app/routes/v2.js
+++ app/routes/v2.js
@@ -8,8 +8,6 @@ router.get('/', async function(req, res, next) {
         res.header('Access-Control-Allow-Origin', 'https://1var.com'); // Replace with your client's URL
         res.header('Access-Control-Allow-Credentials', 'true');
         console.log("vsRouter3")
-        const originalHost = req.headers['x-forwarded-host'];
-        console.log("originalHost",originalHost)
         const type = req.type; // Get the type from the request
         const computeUrl = `https://compute.1var.com/${type}`;
         console.log("type", type);
