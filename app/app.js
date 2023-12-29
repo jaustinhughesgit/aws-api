97f464508fdd93a80ddb35820bf16649b6a535a6
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-29 11:02:55 -0500

    added extra cors headers

--- app/app.js
+++ app/app.js
@@ -12,6 +12,10 @@ var v2Router = require('./routes/v2');
 app.use('/', indexRouter);
 // Route for /cookies/* and /url/*
 app.use('/:type(cookies|url)*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://1var.com');
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header('Access-Control-Allow-Credentials', 'true');
     console.log("req",req)
     req.type = req.params.type; // Capture the type (cookies or url)
     next('route'); // Pass control to the next route
