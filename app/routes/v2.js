a9793cd2aca00427eaf58e6abfe8a16e43d78c92
jaustinhughesgit <jaustinhughes@outlook.com>
2024-01-05 10:11:13 -0500

    removed OPTIONS

--- app/routes/v2.js
+++ app/routes/v2.js
@@ -9,7 +9,7 @@ router.all('/*', async function(req, res, next) {
         res.header('Access-Control-Allow-Origin', 'https://1var.com');
         res.header('Access-Control-Allow-Credentials', 'true');
         res.header('Access-Control-Allow-Headers', 'Content-Type, X-Original-Host');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.header('Access-Control-Allow-Methods', 'GET, POST');
         console.log("vsRouter3")
         const type = req.type; 
         console.log("req.path ==> ",req.apiGateway.event.path)
