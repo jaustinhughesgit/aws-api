cc05b056fcd5155145135e898e8d3bb972cde6bc
jaustinhughesgit <jaustinhughes@outlook.com>
2024-01-29 22:10:12 -0500

    /auth*

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const path = require('path');
 const app = express();
+const axios = require('axios');
 const cookieParser = require('cookie-parser');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,9 +16,49 @@ app.use('/', indexRouter);
 // Route for /cookies/* and /url/*
 app.use('/:type(cookies|url)*', function(req, res, next) {
     console.log("req",req)
+    console.log("req.params.type", req.params.type)
     req.type = req.params.type; // Capture the type (cookies or url)
     next('route'); // Pass control to the next route
 }, v2Router);
+
+app.get("/auth*", async function(req, res, next){
+    console.log("*****")
+    res.header('Access-Control-Allow-Origin', 'https://1var.com');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Original-Host');
+    const reqPath = req.apiGateway.event.path
+    const reqBody = req.body;
+    const accessToken = req.cookies.accessToken;
+    if (req.method === 'GET' || req.method === 'POST') {
+        console.log("reqPath",reqPath)
+        console.log("reqBody",reqBody)
+        console.log("accessToken",accessToken)
+        const originalHost = req.headers['x-original-host'];
+        const computeUrl = `https://compute.1var.com${reqPath}`;
+        const response = await axios.post(computeUrl, { 
+            withCredentials: true,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Original-Host': originalHost,
+                'X-accessToken': accessToken
+            },
+            body: reqBody
+        });
+        
+        const cookies = response.headers['set-cookie'];
+        if (cookies) {
+            cookies.forEach(cookie => {
+                res.append('Set-Cookie', cookie);
+            });
+        }
+        
+        res.send(response.data);
+
+    } else {
+        res.send("")
+    }
+})
 
 // Fallback route for other paths
 app.get('*', (req, res) => {
