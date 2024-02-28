d56ff20fa49f4a753292bf937850c819537fbdae
jaustinhughesgit <jaustinhughes@outlook.com>
2024-02-28 15:56:54 -0500

    asdf

--- app/app.js
+++ app/app.js
@@ -25,7 +25,7 @@ app.all("/auth*", async function(req, res, next){
     console.log("*****")
     res.header('Access-Control-Allow-Origin', 'https://1var.com');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Original-Host','X-accessToken');
+    res.header('Access-Control-Allow-Headers', 'content-type, X-Original-Host','x-accessToken');
 
     if (req.method === 'GET' || req.method === 'POST') {
         const reqPath = req.apiGateway.event.path
