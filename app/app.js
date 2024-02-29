66beafc584702120df50161ca9bfe06cdcfb5427
jaustinhughesgit <jaustinhughes@outlook.com>
2024-02-28 21:50:41 -0500

    asdf

--- app/app.js
+++ app/app.js
@@ -68,6 +68,53 @@ app.all("/auth*", async function(req, res, next){
     }
 })
 
+app.all("/blocks*", async function(req, res, next){
+    console.log("*****")
+    res.header('Access-Control-Allow-Origin', 'https://1var.com');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Original-Host');
+
+    if (req.method === 'GET' || req.method === 'POST') {
+        const reqPath = req.apiGateway.event.path
+        const reqBody = req.body.body;
+        console.log("req.headers",req.headers)
+        const accessToken = req.body.headers['X-accessToken'];
+        const originalHost = req.body.headers['X-Original-Host'];
+        const computeUrl = `https://compute.1var.com/blocks${reqPath}`;
+        console.log("reqPath",reqPath)
+        console.log("reqBody",reqBody)
+        console.log("originalHost", originalHost)
+        console.log("accessToken",accessToken)
+        const response = await axios.post(computeUrl, { 
+            withCredentials: true,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Original-Host': originalHost,
+                'X-accessToken': accessToken
+            },
+            body: reqBody
+        });
+        
+        const cookies = response.headers['set-cookie'];
+        if (cookies) {
+            cookies.forEach(cookie => {
+                res.append('Set-Cookie', cookie);
+            });
+        }
+        console.log("response.data", response.data);
+        console.log("typeof", typeof response.data)
+        let resData = response.data
+        if (typeof response.data == "number"){
+            resData = response.data.toString()
+        }
+        res.send(resData);
+
+    } else {
+        res.send("")
+    }
+})
+
 // Fallback route for other paths
 app.get('*', (req, res) => {
     res.status(404).send('Page not found');
