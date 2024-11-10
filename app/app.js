5a0024696dfc98dd444a9487bf8d92fa49bbcf00
jaustinhughesgit <jaustinhughes@outlook.com>
2024-11-10 09:17:21 -0500

    asdf

--- app/app.js
+++ app/app.js
@@ -60,6 +60,8 @@ app.all("/auth*", async function(req, res, next){
       res.setHeader('Content-Type', 'application/pdf');
       res.setHeader('Content-Disposition', 'attachment; filename=document.pdf');
       console.log("response", response.data)
+      const jsonObject = JSON.parse(response.data.toString('utf8'))
+      console.log("jsonObject", jsonObject)
       //res.send(response.data)
     } else {
       // Set the headers for other response types
